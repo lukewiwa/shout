70fa87c394c772113753b0bbb046e5f4ddf7498d
lukewiwa <luke.wiwa@gmail.com>
2021-07-30 14:05:03 +0000

    Just a completely new deal. Refactor and update to the latest CRA.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    colors: {
+      white: colors.white,
+      grey: colors.gray,
+      orange: colors.orange,
+    },
+    extend: {},
   },
-  variants: {},
-  plugins: []
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
