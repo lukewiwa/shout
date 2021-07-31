5d5757725f1fde8f681bf3460c8ac839494fccdf
lukewiwa <luke.wiwa@gmail.com>
2021-07-31 10:38:35 +0000

    Use a form

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,22 @@ const App: React.FC = () => {
 
 const Talk = (props: TalkProps) => {
   return (
-    <div className="flex flex-col stretch justify-between w-full max-w-2xl self-center p-5 pt-0">
+    <form
+      onSubmit={props.onShout}
+      className="flex flex-col stretch justify-between w-full max-w-2xl self-center p-5 pt-0"
+    >
       <input
         type="text"
         className="bg-white focus:outline-none focus:border-orange-400 border border-grey-300 py-2 px-4 block w-full appearance-none leading-normal"
         value={props.text}
         onChange={props.onTextChange}
       ></input>
-      <button
+      <input
         className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 mt-5"
-        type="button"
-        onClick={props.onShout}
-      >
-        SHOUT!
-      </button>
-    </div>
+        type="submit"
+        value="SHOUT!"
+      />
+    </form>
   );
 };
 
